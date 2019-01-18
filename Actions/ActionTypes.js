c12d3587c808a61f9b25005d9af0a4120c4507af
raviteja20166 <46320554+raviteja20166@users.noreply.github.com>
2019-01-18 16:25:33 +0530

    Create ActionTypes.js

---
+++ Actions/ActionTypes.js
@@ -0,0 +1,5 @@
+// export const COUNTER_INCREMENT = 'counter_increment';
+// export const COUNTER_DECREMENT = 'counter_decrement';
+export const LOGIN = 'login';
+export const SavePhoto = 'SavePhoto';
+// export const LOGOUT = 'logout';
